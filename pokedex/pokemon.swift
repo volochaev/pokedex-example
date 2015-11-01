76044d95ccbe23859265762577e5e66b14e4127f
Nikolai Volochaev <nik@funriver.net>
2015-11-01 15:58:42 +0300

    Add `pokemon` class

---
+++ pokedex/pokemon.swift
@@ -0,0 +1,31 @@
+//
+//  pokemon.swift
+//  pokedex
+//
+//  Created by Nikolai Volochaev on 01/11/15.
+//  Copyright © 2015 Nikolai Volochaev. All rights reserved.
+//
+
+import Foundation
+
+class Pokemon {
+    private var _name: String!
+    private var _pokedexId: Int!
+    
+    var name: String {
+        get {
+            return _name
+        }
+    }
+    
+    var pokedexId: Int {
+        get {
+            return _pokedexId
+        }
+    }
+    
+    init(name: String, pokedexId: Int) {
+        _name = name
+        _pokedexId = pokedexId
+    }
+}
